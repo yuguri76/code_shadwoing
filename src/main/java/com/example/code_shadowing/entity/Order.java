11cb8b3d893e901864f93099eaa965451eba9e3a
yugyuri <ysy9976@naver.com>
2026-04-15 13:03:07 +0900

    order 누락 해결

---
+++ src/main/java/com/example/code_shadowing/entity/Order.java
@@ -0,0 +1,25 @@
+package com.example.code_shadowing.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "orders")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    public Order(Product product) {
+        this.product = product;
+    }
+}
